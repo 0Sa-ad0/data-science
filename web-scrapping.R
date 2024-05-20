1eedb2cb062ab864755d88d50f9ca10ea74597eb
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2024-05-20 17:48:08 +0600

    Add files via upload
    here, it is used "selector gadget" chrome extension for selecting one or multiple columns.

---
+++ web-scrapping.R
@@ -0,0 +1,12 @@
+install.packages("rvest")
+install.packages("dplyr")
+library(rvest)
+library(dplyr)
+url <- "https://fbref.com/en/countries/"
+page <- read_html(url)
+countries <- page %>% html_nodes("th.left") %>%  html_text()
+countries
+governing_body <- page %>% html_nodes(".left.poptip") %>%  html_text()
+governing_body
+ds <- data.frame(countries, governing_body)
+write.csv(ds,"ok.csv",row.names=FALSE)
